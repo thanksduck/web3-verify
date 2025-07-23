1db3d53644b261bd13037187980fdd14bd60aca9
sivm99 <git.v7gr4@simplelogin.fr>
2025-07-23 19:17:13 +0530

    use the port varaible now

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { app } from "./app";
+import { PORT } from "./env";
 
 Bun.dns.prefetch("https://bsc-dataseed1.binance.org/");
 Bun.dns.prefetch("https://bsc-dataseed2.binance.org/");
@@ -7,7 +8,7 @@ Bun.dns.prefetch("https://bsc-dataseed4.binance.org/");
 Bun.dns.prefetch("https://bsc-dataseed1.defibit.io/");
 Bun.dns.prefetch("https://bsc-dataseed2.defibit.io/");
 
-app.listen(7000);
+app.listen(PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
